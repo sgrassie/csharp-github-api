ef777bda5d27433b10db38be01dc49095a684f8e
Stuart Grassie <sgrassie@gmail.com>
2010-08-20 14:09:26 +0100

    Initial commit

---
+++ csharp-github-api.IntegrationTests/GitHubAuthenticatorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using RestSharp;
+
+namespace csharp_github_api.IntegrationTests
+{
+    [TestFixture]
+    public class GitHubAuthenticatorTests
+    {
+        [Test]
+        public void MakeAuthenticatedRequest()
+        {
+            var client = new RestClient
+                             {
+                                 BaseUrl = "http://github.com/api/v2/json",
+                                 Authenticator = new GitHubAuthenticator("", "", false)
+                             };
+
+            var request = new RestRequest
+                               {
+                                   Resource = "/user/show/sgrassie"
+                               };
+
+            var response = client.Execute(request);
+
+            Assert.That(response.Content, Is.StringContaining("total_private_repo_count"));
+        }
+    }
+}
---
+++ csharp-github-api/GitHubAuthenticator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace csharp_github_api
+{
+    public class GitHubAuthenticator : IAuthenticator
+    {
+        private readonly string _username;
+        private readonly string _password;
+        private readonly bool _useApiKey;
+
+        public GitHubAuthenticator(string username, string password, bool useApiKey)
+        {
+            _username = username;
+            _password = password;
+            _useApiKey = useApiKey;
+        }
+
+        public void Authenticate(RestRequest request)
+        {
+            if (!request.Parameters.Any(p => p.Name.Equals("Authorization", StringComparison.InvariantCultureIgnoreCase)))
+            {
+                var token = _useApiKey 
+                                ? Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}/token:{1}", _username, _password))) 
+                                : Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", _username, _password)));
+                var authHeader = string.Format("Basic {0}", token);
+                request.AddParameter("Authorization", authHeader, ParameterType.HttpHeader);
+            }
+        }
+    }
+}
