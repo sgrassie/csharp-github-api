76789fb30888a81ff6cc24e46696865cf1672d17
sgrassie <sgrassie@gmail.com>
2010-08-22 22:18:13 +0100

    Added comments which explain how I modified the RestSharp HttpBasicAuthenticator to work with GitHub's api token authentication.

--- csharp-github-api/GitHubAuthenticator.cs
+++ csharp-github-api/GitHubAuthenticator.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using RestSharp;
-
-namespace csharp_github_api
+﻿namespace csharp_github_api
 {
+    using System;
+    using System.Linq;
+    using System.Text;
+    using RestSharp;
+
+    /// <summary>
+    /// A custom implementation of a RestSharp <see cref="IAuthenticator"/>. It is basically the same as RestSharp's
+    /// <see cref="HttpBasicAuthenticator" />, but it add's the facility to authenticate with a GitHub api token.
+    /// </summary>
     public class GitHubAuthenticator : IAuthenticator
     {
         private readonly string _username;
         private readonly string _password;
         private readonly bool _useApiKey;
 
+        /// <summary>
+        /// Instantiates a new instance of the <see cref="GitHubAuthenticator"/> class.
+        /// </summary>
+        /// <param name="username">The GitHub.com username to login as.</param>
+        /// <param name="password">The password for the user. If logging in with an api key, set the password as the token.</param>
+        /// <param name="useApiKey">A value which indicates whether or not a GitHub.com api key is being used for authentication.</param>
         public GitHubAuthenticator(string username, string password, bool useApiKey)
         {
             _username = username;
@@ -18,6 +28,10 @@ namespace csharp_github_api
             _useApiKey = useApiKey;
         }
 
+        /// <summary>
+        /// Authenticates the request.
+        /// </summary>
+        /// <param name="request">The request to authenticate.</param>
         public void Authenticate(RestRequest request)
         {
             if (!request.Parameters.Any(p => p.Name.Equals("Authorization", StringComparison.InvariantCultureIgnoreCase)))
