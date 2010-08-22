98872271b121cae9b6187f6fdcad1f1eb85c4054
sgrassie <sgrassie@gmail.com>
2010-08-22 22:04:16 +0100

    User model, maps to a github.com user.

---
+++ csharp-github-api/Models/Plan.cs
@@ -0,0 +1,60 @@
+﻿
+namespace csharp_github_api.Models
+{
+    /// <summary>
+    /// Maps to an authenticated users plan
+    /// </summary>
+    public class Plan
+    {
+        /// <summary>
+        /// Gets or sets the name of the GitHub plan.
+        /// </summary>
+        /// <remarks>
+        /// One of: Free, Micro, Small, Medium
+        /// </remarks>
+        public virtual string Name { get; set;}
+
+        /// <summary>
+        /// Gets or sets the number of collaborators in the private plan
+        /// </summary>
+        public virtual int Collaborators { get; set;}
+
+        /// <summary>
+        /// Gets or sets the amount of disk space in the private plan.
+        /// </summary>
+        public virtual long Space { get; set;}
+
+        /// <summary>
+        /// Gets or sets the number of private repositories the user has.
+        /// </summary>
+        public virtual int PrivateRepos { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Plan)
+            {
+                var compareTo = (Plan) obj;
+
+                if (compareTo.Equals(Name) && compareTo.Collaborators.Equals(Collaborators)
+                    && compareTo.Space.Equals(Space) && compareTo.PrivateRepos.Equals(PrivateRepos))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Collaborators.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ csharp-github-api/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharp_github_api.Models
+{
+    /// <summary>
+    /// Represents a GitHub.com user account.
+    /// </summary>
+    public class User
+    {
+        /* public, authentication not required */
+        public virtual int Id { get; set; }
+        public virtual string Login { get; set; }
+        public virtual string Name { get; set;}
+        public virtual string Company { get; set;}
+        public virtual string Location { get; set;}
+        public virtual string Email { get; set;}
+        public virtual string Blog { get; set;}
+        public virtual int FollowingCount { get; set;}
+        public virtual int FollowersCount { get; set;}
+        public virtual int PublicGistCount { get; set;}
+        public virtual int PublicRepoCount { get; set;}
+
+        /*private, authentication required */
+        public virtual int TotalPrivateRepoCount { get; set;}
+        public virtual int Collaborators { get; set;}
+        public virtual long DiskUsage { get; set;}
+        public virtual int OwnedPrivateRepoCount { get; set;}
+        public virtual int PrivateGistCount { get; set;}
+        public virtual Plan Plan { get; set;}
+
+    }
+}
