ba18a98fd4f915f6ffb69db1d8d021b984860909
sgrassie <sgrassie@gmail.com>
2010-08-24 23:52:47 +0100

    Handle Kayak framwork's Invoking event - here we can catch the total number of requests made, as well as do some basic authentication.

--- csharp-github-api.IntegrationTests/IntegrationTestBase.cs
+++ csharp-github-api.IntegrationTests/IntegrationTestBase.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using Kayak;
 using Kayak.Framework;
 
@@ -8,11 +10,35 @@ namespace csharp_github_api.IntegrationTests
     {
         protected readonly KayakServer WebServer = new KayakServer();
         protected string BaseUrl = "http://localhost:8080";
+        protected int RequestCount;
 
         protected IntegrationTestBase()
         {
-            WebServer.UseFramework();
+            var framework = WebServer.UseFramework();
+            framework.Invoking += framework_Invoking;
             WebServer.Start();
+
+        }
+
+        void framework_Invoking(object sender, InvokingEventArgs e)
+        {
+            RequestCount++;
+
+            var header = e.Invocation.Context.Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(header))
+            {
+                var parts = Encoding.ASCII.GetString(Convert.FromBase64String(header.Substring("Basic ".Length))).Split(':');
+
+                if (parts.Count() == 2)
+                {
+                    // Purely an aid to unit testing.
+                    e.Invocation.Context.Response.Headers.Add("Authenticated", "true");
+
+                    // We don't want to write anything here because it will interfere with our json response from the Services
+                    //e.Invocation.Context.Response.Write(string.Join("|", parts));
+                }
+            }
         }
 
         public void Dispose()
