7fae34936b888605846c3491bf5088205e2a00e9
Stuart Grassie <sgrassie@gmail.com>
2010-08-25 13:04:58 +0100

    Future test proofing: Added a method of tracking whether the API request limit has been exceeded.

--- csharp-github-api.IntegrationTests/IntegrationTestBase.cs
+++ csharp-github-api.IntegrationTests/IntegrationTestBase.cs
@@ -9,8 +9,11 @@ namespace csharp_github_api.IntegrationTests
     public abstract class IntegrationTestBase : IDisposable
     {
         protected readonly KayakServer WebServer = new KayakServer();
+        protected readonly int GitHubApiRequestLimit = 60;
         protected string BaseUrl = "http://localhost:8080";
         protected int RequestCount;
+        private DateTime _timeOfPreviousRequest;
+        private DateTime _timeOfCurrentRequest;
 
         protected IntegrationTestBase()
         {
@@ -23,20 +26,49 @@ namespace csharp_github_api.IntegrationTests
         void framework_Invoking(object sender, InvokingEventArgs e)
         {
             RequestCount++;
+            _timeOfCurrentRequest = DateTime.Now;
 
-            var header = e.Invocation.Context.Request.Headers["Authorization"];
-
-            if (!string.IsNullOrEmpty(header))
+            if (!HasExceededApiLimit())
             {
-                var parts = Encoding.ASCII.GetString(Convert.FromBase64String(header.Substring("Basic ".Length))).Split(':');
+                var header = e.Invocation.Context.Request.Headers["Authorization"];
 
-                if (parts.Count() == 2)
+                if (!string.IsNullOrEmpty(header))
                 {
-                    /* On some requests, if you are authenticated, you get extra information back in the response,
-                     * or are allowed to modify/create things,so with this we can simulate that in the Services */
-                    e.Invocation.Context.Response.Headers.Add("Authenticated", "true");
+                    var parts =
+                        Encoding.ASCII.GetString(Convert.FromBase64String(header.Substring("Basic ".Length))).Split(':');
+
+                    if (parts.Count() == 2)
+                    {
+                        /* On some requests, if you are authenticated, you get extra information back in the response,
+                         * or are allowed to modify/create things,so with this we can simulate that in the Services */
+                        e.Invocation.Context.Response.Headers.Add("Authenticated", "true");
+                    }
                 }
             }
+            else
+            {
+                e.Invocation.Context.Response.SetStatusToForbidden(); // Github.com would actually return access denied
+            }
+        }
+
+        /// <summary>
+        /// Determines whether or not the API request limit has been exceeded.
+        /// </summary>
+        /// <returns><c>True</c> if the API limit has been exceeded; otherwise returns <c>false</c>.</returns>
+        protected virtual bool HasExceededApiLimit()
+        {
+            var timeSpan = _timeOfCurrentRequest.Subtract(_timeOfPreviousRequest);
+
+            if ((timeSpan.TotalMinutes < 1.0d) && (RequestCount >= GitHubApiRequestLimit))
+            {
+                _timeOfPreviousRequest = _timeOfCurrentRequest;
+
+                return true;
+            }
+
+            _timeOfPreviousRequest = _timeOfCurrentRequest;
+
+            return false;
         }
 
         public void Dispose()
