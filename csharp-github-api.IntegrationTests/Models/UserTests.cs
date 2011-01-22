147826db407d846335b68c9376ad3087fdc14020
sgrassie <sgrassie@gmail.com>
2011-01-22 08:40:15 +0000

    Tests for the User model.

---
+++ csharp-github-api.IntegrationTests/Models/UserTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace csharp_github_api.IntegrationTests.Models
+{
+    [TestFixture]
+    public class UserTests
+    {
+        private Github _github;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _github = new Github("http://github.com/api/v2/json", @"C:\development\secretpasswordfile.xml");    
+        }
+
+        [Test]
+        public void Should_Get_List_Of_Following()
+        {
+            var result = _github.User.GetUser("sgrassie").Following;
+
+            Assert.That(result, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void Should_Get_List_Of_Followers()
+        {
+            var result = _github.User.GetUser("sgrassie").Followers;
+
+            Assert.That(result, Has.Count.GreaterThan(0));
+        }
+    }
+}
