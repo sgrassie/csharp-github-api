86f2ae46b005693000daeb0b49b37cb6ae2ddce4
Stuart Grassie <sgrassie@gmail.com>
2012-12-11 23:21:17 +0000

    Package updates
    Update Nuget packages for all projects. Added NUnit to the test projects.
    
    Added SolutionVersion.cs

---
+++ SolutionVersion.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyCompany("TemporalCohesion.co.uk")]
+[assembly: AssemblyVersionAttribute("0.0.0.01")]
+[assembly: AssemblyFileVersionAttribute("0.0.0.01")]
+[assembly: AssemblyInformationalVersionAttribute("0.0.0.1")]
+[assembly: AssemblyTitleAttribute("")]
+[assembly: AssemblyProductAttribute("")]
+[assembly: AssemblyDescriptionAttribute("")]
+[assembly: AssemblyTrademarkAttribute(" ")]
+[assembly: AssemblyCopyrightAttribute("")]
