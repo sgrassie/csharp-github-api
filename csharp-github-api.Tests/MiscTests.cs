3f91c927b1698556c40cb1057e865ba5efc5a37f
Stuart Grassie <sgrassie@gmail.com>
2012-12-14 13:06:47 +0000

    Spike out returing a dynamic object
    Resharper uses SimpleJson, so we can too, because it's a single source file dropin, and we can make it internal.
    
    I've tried two ways of implementing this:
    
    1. Use a custom IDeserializer
    2. Use an extension method to hook into IRestResponse
    
    Of the two, I prefer the 2nd, as it looks much cleaner.

---
+++ csharp-github-api.Tests/MiscTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Deserializers;
+using csharp_github_api.Extensions;
+using csharp_github_api.Models;
+
+namespace csharp_github_api.Tests
+{
+    [TestFixture]
+    public class MiscTests
+    {
+        [Test]
+        public void DynamicResponse_WithCustomDeserializer()
+        {
+            var restRequest = new RestRequest
+            {
+                Resource = "/users/sgrassie"
+            };
+
+            var client = new RestClient
+            {
+                BaseUrl = "https://api.github.com"
+            };
+            client.AddHandler("application/json", new DynamicJsonDeserializer());
+
+            dynamic response = client.Execute<dynamic>(restRequest);
+            
+            Assert.That(response.Data["login"], Is.StringMatching("sgrassie"));
+        }
+
+        [Test]
+        public void DynamicReponse_WithExtensionMethod()
+        {
+            var restRequest = new RestRequest
+            {
+                Resource = "/users/sgrassie"
+            };
+
+            var client = new RestClient
+            {
+                BaseUrl = "https://api.github.com"
+            };
+
+            var response = client.Execute(restRequest);
+            var dynamicPart = response.Dynamic();
+
+            Assert.That(dynamicPart.login, Is.StringMatching("sgrassie"));
+        }
+    }
+
+    public class DynamicJsonDeserializer : IDeserializer
+    {
+        public string RootElement { get; set; }
+
+        public string Namespace { get; set; }
+
+        public string DateFormat { get; set; }
+
+        public T Deserialize<T>(IRestResponse response)
+        {
+            return (T)SimpleJson.DeserializeObject(response.Content);
+        }
+    }
+}
